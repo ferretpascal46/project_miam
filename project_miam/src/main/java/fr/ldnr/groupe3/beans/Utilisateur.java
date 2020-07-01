e385ee7c9bad356f68cb15902b86db0f7009277a
Sarah <sarahbettini81@gmail.com>
2020-07-01 11:13:41 +0200

    création beans Utilisateur

---
+++ project_miam/src/main/java/fr/ldnr/groupe3/beans/Utilisateur.java
@@ -0,0 +1,62 @@
+package fr.ldnr.groupe3.beans;
+
+import java.io.Serializable;
+
+public class Utilisateur implements Serializable{
+	// Les propriétés du bean : elles correspondent aux champs de la base de données
+    private int idUtilisateur;
+    private String adresseMail;
+    private String motDePasse;
+    private int role;
+    
+    public Utilisateur() {
+    }
+    
+	public Utilisateur(int idUtilisateur, String adresseMail, String motDePasse, int role) {
+		super();
+		this.idUtilisateur = idUtilisateur;
+		this.adresseMail = adresseMail;
+		this.motDePasse = motDePasse;
+		this.role = role;
+	}
+
+	public int getIdUtilisateur() {
+		return idUtilisateur;
+	}
+
+	public void setIdUtilisateur(int idUtilisateur) {
+		this.idUtilisateur = idUtilisateur;
+	}
+
+	public String getAdresseMail() {
+		return adresseMail;
+	}
+
+	public void setAdresseMail(String adresseMail) {
+		this.adresseMail = adresseMail;
+	}
+
+	public String getMotDePasse() {
+		return motDePasse;
+	}
+
+	public void setMotDePasse(String motDePasse) {
+		this.motDePasse = motDePasse;
+	}
+
+	public int getRole() {
+		return role;
+	}
+
+	public void setRole(int role) {
+		this.role = role;
+	}
+	
+	@Override
+    public String toString() {
+        return "Utilisateur{" + "id=" + idUtilisateur + ", email=" + adresseMail + ", motDePasse=" + motDePasse + ", role=" + role + '}';
+    }
+    
+	
+	
+}
