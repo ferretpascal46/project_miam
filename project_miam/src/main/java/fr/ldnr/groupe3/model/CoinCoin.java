7c30d824e12a04d9aa171adb39b3e0c741d71622
Seguy-PierreJean <67579372+Seguy-PierreJean@users.noreply.github.com>
2020-07-01 10:51:14 +0200

    Creation et Test de la fonction de Hashage de mot de Passe

--- project_miam/src/main/java/fr/ldnr/groupe3/model/CoinCoin.java
+++ project_miam/src/main/java/fr/ldnr/groupe3/model/CoinCoin.java
@@ -1,10 +1,47 @@
 package fr.ldnr.groupe3.model;
 
+import fr.ldnr.group3.forms.HachForm;
+
 public class CoinCoin {
 
 	public static void main(String[] args) {
-		System.out.println("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA");
-
+		
+		String mdp = "coincoin";
+		String adresseMail = "pj.sequi@gmail.com";
+		String hashedPassword = "";
+		HachForm hf = new HachForm();
+		
+		//essai 1
+		try {
+			 hashedPassword = hf.hach(adresseMail, mdp);
+		} catch (Exception e) {
+			System.out.println("Erreur lors du hash : " + e);
+		}
+		
+		System.out.println("mdp = " + mdp);
+		System.out.println("hasedPassWord = " + hashedPassword);
+		
+		//essai identique
+		try {
+			 hashedPassword = hf.hach(adresseMail, mdp);
+		} catch (Exception e) {
+			System.out.println("Erreur lors du hash : " + e);
+		}
+		
+		System.out.println("mdp = " + mdp);
+		System.out.println("hasedPassWord = " + hashedPassword);
+		
+		
+		//essai different
+		mdp = "coincoi";
+		try {
+			 hashedPassword = hf.hach(adresseMail, mdp);
+		} catch (Exception e) {
+			System.out.println("Erreur lors du hash : " + e);
+		}
+		
+		System.out.println("mdp = " + mdp);
+		System.out.println("hasedPassWord = " + hashedPassword);
 	}
-
 }
+ 
