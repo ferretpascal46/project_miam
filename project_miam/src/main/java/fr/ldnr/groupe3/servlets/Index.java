badb04c1ef5f1f2a22bb9f5e3a041cbd712d2441
Pascal <ferretpascal46@gmail.com>
2020-07-06 11:59:17 +0200

    recuperer id sur index

--- project_miam/src/main/java/fr/ldnr/groupe3/servlets/Index.java
+++ project_miam/src/main/java/fr/ldnr/groupe3/servlets/Index.java
@@ -57,6 +57,11 @@ public class Index extends HttpServlet {
 	protected void doPost(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 		// TODO Auto-generated method stub
+		String idProduit = request.getParameter("idProduit");
+
+		System.out.println(idProduit);
+		
+		
 		doGet(request, response);
 	}
 
